f72180c933409dae8f63ef629602502f66439519
Khushraj Rathod <khushrajrathod@gmail.com>
2020-04-28 13:39:17 +0530

    Add support for signing apks

---
+++ sign.sh
@@ -0,0 +1,9 @@
+LD_LIBRARY_PATH=./signapk/ java -jar ./signapk/signapk.jar -a 4096\
+	./signapk/platform.x509.pem \
+	./signapk/platform.pk8 \
+	binder32/ims/ims/dist/ims.apk binder64/ims/ims.apk
+
+LD_LIBRARY_PATH=./signapk/ java -jar ./signapk/signapk.jar -a 4096\
+  ./signapk/platform.x509.pem \
+  ./signapk/platform.pk8 \
+  binder64/ims/ims/dist/ims.apk binder64/ims/ims.apk
