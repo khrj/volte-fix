6659159e41a12701ca3a973304972731a0eb3cc6
Khushraj Rathod <khushrajrathod@gmail.com>
2020-04-28 15:35:48 +0530

    Added ims enabling properties in install.sh

--- install.sh
+++ install.sh
@@ -11,4 +11,8 @@ adb root
 adb remount
 adb push $scriptdir/binder${1}/ims /system/priv-app/
 adb push $scriptdir/android.hardware.telephony.ims.xml /system/etc/permissions
+adb shell setprop persist.dbg.allow_ims_off 1
+adb shell setprop persist.dbg.volte_avail_ovr 1
+adb shell setprop persist.dbg.vt_avail_ovr 1
+adb shell setprop persist.dbg.wfc_avail_ovr 1
 echo "REBOOT YOUR PHONE TWICE TO ACTIVATE VoLTE"
