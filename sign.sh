d450d51f432aeca5bed492e9cb114b40a3d0bca5
Khushraj Rathod <khushrajrathod@gmail.com>
2020-04-28 13:53:31 +0530

    Reformat build script, sign binder32 apk

--- sign.sh
+++ sign.sh
@@ -1,9 +1,10 @@
 LD_LIBRARY_PATH=./signapk/ java -jar ./signapk/signapk.jar -a 4096\
-	./signapk/platform.x509.pem \
-	./signapk/platform.pk8 \
-	binder32/ims/ims/dist/ims.apk binder64/ims/ims.apk
+  ./signapk/platform.x509.pem \
+  ./signapk/platform.pk8 \
+  binder64/ims/ims/dist/ims.apk binder64/ims/ims.apk
 
 LD_LIBRARY_PATH=./signapk/ java -jar ./signapk/signapk.jar -a 4096\
   ./signapk/platform.x509.pem \
   ./signapk/platform.pk8 \
-  binder64/ims/ims/dist/ims.apk binder64/ims/ims.apk
+  binder32/ims/ims/dist/ims.apk binder32/ims/ims.apk
+
