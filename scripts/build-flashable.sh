855b18cca12387db579f53942fcaecff9d7f8c73
Khushraj Rathod <khushrajrathod@gmail.com>
2020-06-04 23:14:43 +0530

    Add uninstaller zip

--- scripts/build-flashable.sh
+++ scripts/build-flashable.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Copyright 2020 Khushraj Rathod
 #
 # This file is part of VoLTE-Fix.
@@ -17,14 +18,33 @@
 
 scriptdir=$(cd ./"$(dirname "$0")"/ || exit; pwd)
 
-cd $scriptdir/.. || exit
+cd "$scriptdir"/.. || exit
 
+# First let's make the installer
+# Make a copy
 cp -rf ./FlashableZip/ ./zip-temp/
+# Remove the uninstaller script, we used don't need that now
+rm -f ./zip-temp/META-INF/com/google/android/update-binary-uninstall
+# Rename the installer script
+mv ./zip-temp/META-INF/com/google/android/update-binary-install ./zip-temp/META-INF/com/google/android/update-binary
+# Add files to install
 mkdir -p ./zip-temp/system64/priv-app/
 mkdir -p ./zip-temp/system32/priv-app/
-
 cp -rf ./64bit/ims ./zip-temp/system64/priv-app/
 cp -rf ./32bit/ims ./zip-temp/system32/priv-app/
+# Make installer zip
 (cd ./zip-temp && zip -0ry ../GSI-Qualcomm-VoLTE-Fix.zip . -x "*.DS_Store")
+# Cleanup
+rm -rf ./zip-temp/
 
+# Now let's make the uninstaller
+# Make a copy
+cp -rf ./FlashableZip/ ./zip-temp/
+# Remove the installer script, we used that earlier
+rm -f ./zip-temp/META-INF/com/google/android/update-binary-install
+# Rename the uninstaller script
+mv ./zip-temp/META-INF/com/google/android/update-binary-uninstall ./zip-temp/META-INF/com/google/android/update-binary
+# Make uninstaller zip
+(cd ./zip-temp && zip -0ry ../VoLTE-Fix-Uninstaller.zip . -x "*.DS_Store")
+# Cleanup
 rm -rf ./zip-temp/
